8104c94905a01262fab717ceca98522e1ec8907a
ohsang14 <132821751+ohsang14@users.noreply.github.com>
2023-09-12 02:29:45 +0900

    ok

--- JavaScript  그림판/MEME-MAKER/app.js
+++ JavaScript  그림판/MEME-MAKER/app.js
@@ -1,11 +1,8 @@
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-
-ctx.rect(50,50,100,100);
-ctx.rect(150,150,100,100);
-ctx.rect(250,250,100,100);
-ctx.fill();
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(400, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
