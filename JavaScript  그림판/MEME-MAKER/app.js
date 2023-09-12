d6165f35f97dde69baabf0ab34989cf02f95d2a3
ohsang <ast6108@naver.com>
2023-09-12 18:58:15 +0900

    test

---
+++ JavaScript  그림판/MEME-MAKER/app.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(400, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
